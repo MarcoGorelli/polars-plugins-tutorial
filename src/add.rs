45eab61da028f9500b4d4e89e3948e0a1728704b
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-01-11 14:57:49 +0000

    clean

--- src/add.rs
+++
@@ -1,9 +0,0 @@
-use polars::prelude::arity::binary_elementwise;
-use polars::prelude::*;
-
-pub(crate) fn impl_add(left: &Int64Chunked, right: &Int64Chunked) -> Int64Chunked {
-    binary_elementwise(left, right, |left, right| match (left, right) {
-        (Some(left), Some(right)) => Some(left + right),
-        _ => None,
-    })
-}

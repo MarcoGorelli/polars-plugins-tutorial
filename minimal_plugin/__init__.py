b24742a10ea94bdf08e22cf9f846230ca050d69e
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-01-29 16:33:50 +0000

    fixup

--- minimal_plugin/__init__.py
+++ minimal_plugin/__init__.py
@@ -6,66 +6,6 @@ from minimal_plugin.utils import parse_into_expr
 lib = _get_shared_lib_location(__file__)
 
 
-<<<<<<< HEAD
-def noop(expr: str | pl.Expr) -> pl.Expr:
-    if isinstance(expr, str):
-        expr = pl.col(expr)
-    return expr.register_plugin(
-        lib=lib,
-        symbol="noop",
-        is_elementwise=True,
-    )
-
-def abs_i64(expr: str | pl.Expr) -> pl.Expr:
-    if isinstance(expr, str):
-        expr = pl.col(expr)
-    return expr.register_plugin(
-        lib=lib,
-        symbol="abs_i64",
-        is_elementwise=True,
-    )
-
-def abs_numeric(expr: str | pl.Expr) -> pl.Expr:
-    if isinstance(expr, str):
-        expr = pl.col(expr)
-    return expr.register_plugin(
-        lib=lib,
-        symbol="abs_numeric",
-        is_elementwise=True,
-    )
-
-def sum_i64(expr: str | pl.Expr, other: IntoExpr) -> pl.Expr:
-    if isinstance(expr, str):
-        expr = pl.col(expr)
-    return expr.register_plugin(
-        lib=lib,
-        symbol="sum_i64",
-        is_elementwise=True,
-        args=[other]
-    )
-
-def cum_sum(expr: str | pl.Expr) -> pl.Expr:
-    if isinstance(expr, str):
-        expr = pl.col(expr)
-    return expr.register_plugin(
-        lib=lib,
-        symbol="cum_sum",
-        is_elementwise=False,
-    )
-
-def pig_latinnify(expr: str | pl.Expr) -> pl.Expr:
-    if isinstance(expr, str):
-        expr = pl.col(expr)
-    return expr.register_plugin(
-        lib=lib,
-        symbol="pig_latinnify",
-        is_elementwise=True,
-    )
-
-def abs_i64_fast(expr: str | pl.Expr) -> pl.Expr:
-    if isinstance(expr, str):
-        expr = pl.col(expr)
-=======
 
 def noop(expr: IntoExpr) -> pl.Expr:
     expr = parse_into_expr(expr)
@@ -118,21 +58,14 @@ def pig_latinnify(expr: IntoExpr) -> pl.Expr:
 
 def abs_i64_fast(expr: IntoExpr) -> pl.Expr:
     expr = parse_into_expr(expr)
->>>>>>> origin/main
     return expr.register_plugin(
         lib=lib,
         symbol="abs_i64_fast",
         is_elementwise=True,
     )
     
-<<<<<<< HEAD
-def add_suffix(expr: str | pl.Expr, *, suffix: str) -> pl.Expr:
-    if isinstance(expr, str):
-        expr = pl.col(expr)
-=======
 def add_suffix(expr: IntoExpr, *, suffix: str) -> pl.Expr:
     expr = parse_into_expr(expr)
->>>>>>> origin/main
     return expr.register_plugin(
         lib=lib,
         symbol="add_suffix",
@@ -140,43 +73,27 @@ def add_suffix(expr: IntoExpr, *, suffix: str) -> pl.Expr:
         kwargs={"suffix": suffix}
     )
 
-<<<<<<< HEAD
-def snowball_stem(expr: str | pl.Expr) -> pl.Expr:
-    if isinstance(expr, str):
-        expr = pl.col(expr)
-=======
 def snowball_stem(expr: IntoExpr) -> pl.Expr:
     expr = parse_into_expr(expr)
->>>>>>> origin/main
     return expr.register_plugin(
         lib=lib,
         symbol="snowball_stem",
         is_elementwise=True,
     )
 
-<<<<<<< HEAD
-def weighted_mean(expr: str | pl.Expr, weights: IntoExpr) -> pl.Expr:
-    if isinstance(expr, str):
-        expr = pl.col(expr)
-=======
 def weighted_mean(expr: IntoExpr, weights: IntoExpr) -> pl.Expr:
     expr = parse_into_expr(expr)
->>>>>>> origin/main
     return expr.register_plugin(
         lib=lib,
         symbol="weighted_mean",
         is_elementwise=True,
         args=[weights]
     )
-<<<<<<< HEAD
 
-def shift_struct(expr: str | pl.Expr) -> pl.Expr:
-    if isinstance(expr, str):
-        expr = pl.col(expr)
+def shift_struct(expr: IntoExpr) -> pl.Expr:
+    expr = parse_into_expr(expr)
     return expr.register_plugin(
         lib=lib,
         symbol="shift_struct",
         is_elementwise=True,
     )
-=======
->>>>>>> origin/main

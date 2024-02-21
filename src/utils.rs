2f7ccf2c4704b733c2fd81a16898352930db5826
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-02-21 11:03:39 +0000

    wip, not sure if I want to commit this

--- src/utils.rs
+++ src/utils.rs
@@ -45,38 +45,38 @@ where
 // 
 // in your `Cargo.toml` file.
 // Only uncomment if needed
-// pub(crate) fn binary_apply_to_buffer_generic<T, K, F>(
-//     lhs: &ChunkedArray<T>,
-//     rhs: &ChunkedArray<K>,
-//     mut f: F,
-// ) -> StringChunked
-// where
-//     T: PolarsDataType,
-//     K: PolarsDataType,
-//     F: for<'a> FnMut(T::Physical<'a>, K::Physical<'a>) -> String,
-// {
-//     let (lhs, rhs) = align_chunks_binary(lhs, rhs);
-//     let chunks = lhs
-//         .downcast_iter()
-//         .zip(rhs.downcast_iter())
-//         .map(|(lhs_arr, rhs_arr)| {
-//             let mut buf = String::new();
-//             let mut mutarr = MutablePlString::with_capacity(lhs_arr.len());
+pub(crate) fn binary_apply_to_buffer_generic<T, K, F>(
+    lhs: &ChunkedArray<T>,
+    rhs: &ChunkedArray<K>,
+    mut f: F,
+) -> StringChunked
+where
+    T: PolarsDataType,
+    K: PolarsDataType,
+    F: for<'a> FnMut(T::Physical<'a>, K::Physical<'a>) -> String,
+{
+    let (lhs, rhs) = align_chunks_binary(lhs, rhs);
+    let chunks = lhs
+        .downcast_iter()
+        .zip(rhs.downcast_iter())
+        .map(|(lhs_arr, rhs_arr)| {
+            let mut buf = String::new();
+            let mut mutarr = MutablePlString::with_capacity(lhs_arr.len());
 
-//             for (lhs_opt_val, rhs_opt_val) in lhs_arr.iter().zip(rhs_arr.iter()) {
-//                 match (lhs_opt_val, rhs_opt_val) {
-//                     (Some(lhs_val), Some(rhs_val)) => {
-//                         let res = f(lhs_val, rhs_val);
-//                         buf.clear();
-//                         write!(buf, "{res}").unwrap();
-//                         mutarr.push(Some(&buf))
-//                     }
-//                     _ => mutarr.push_null(),
-//                 }
-//             }
+            for (lhs_opt_val, rhs_opt_val) in lhs_arr.iter().zip(rhs_arr.iter()) {
+                match (lhs_opt_val, rhs_opt_val) {
+                    (Some(lhs_val), Some(rhs_val)) => {
+                        let res = f(lhs_val, rhs_val);
+                        buf.clear();
+                        write!(buf, "{res}").unwrap();
+                        mutarr.push(Some(&buf))
+                    }
+                    _ => mutarr.push_null(),
+                }
+            }
 
-//             mutarr.freeze().boxed()
-//         })
-//         .collect();
-//     unsafe { ChunkedArray::from_chunks("placeholder", chunks) }
-// }
+            mutarr.freeze().boxed()
+        })
+        .collect();
+    unsafe { ChunkedArray::from_chunks("placeholder", chunks) }
+}
